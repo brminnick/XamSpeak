ebb26efaabcee005a45c8b4e3a30086ad02e47f6
brminnick <brandon.r.minnick@gmail.com>
2017-05-04 17:46:08 -0500

    Added Cognitive Services Constants

---
+++ XamSpeak/Constants/CognitiveServicesConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace XamSpeak
+{
+	public static class CognitiveServicesConstants
+	{
+#error Computer Vision API Key and Bing Spell Check API Key Missing! Sign up for a free API Key
+		//Sign up for a free Bing Spell Check API Key: https://www.microsoft.com/cognitive-services/en-us/bing-spell-check-api
+		//Sign up for a free Computer Vision API Key: https://www.microsoft.com/cognitive-services/en-us/computer-vision-api
+		public const string ComputerVisionAPIKey = "Enter Computer Vision API Key Here";
+		public const string BingSpellCheckAPIKey = "Enter Bing Spell Check API Key Here";
+	}
+}

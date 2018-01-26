223cf3bba631cc19ca3c9b8658690dfa9a8b0961
brminnick <brandon.r.minnick@gmail.com>
2018-01-26 10:48:09 -0800

    Updated NuGet Packages, Refactored Folder Names

--- Source/XamSpeak/Constants/CognitiveServicesConstants.cs
+++ Source/XamSpeak/Constants/CognitiveServicesConstants.cs
@@ -1,12 +1,12 @@
 ﻿namespace XamSpeak
 {
-	public static class CognitiveServicesConstants
-	{
-		#warning Add your free Cognitive Services API Keys
+    public static class CognitiveServicesConstants
+    {
+#warning Add your free Cognitive Services API Keys
         //Sign up for a free Computer Vision API Key: https://aka.ms/yqx7kz
         public const string ComputerVisionAPIKey = "682f3d1f175a4b82b20d79078af0ccae";
-		
-	//Sign up for a free Bing Spell Check API Key: https://aka.ms/Uis4df	
+
+        //Sign up for a free Bing Spell Check API Key: https://aka.ms/Uis4df	
         public const string BingSpellCheckAPIKey = "30cf90049cdc4516a06c063769edcb20";
-	}
+    }
 }

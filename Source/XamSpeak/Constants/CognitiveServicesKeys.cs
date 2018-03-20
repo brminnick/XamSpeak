5c09a20420b52ae2b2ba710087e30a4717e5637e
Brandon Minnick <brandon.r.minnick@gmail.com>
2018-03-20 15:50:25 -0400

    Added SpeakerRecognitionAPIKey

--- Source/XamSpeak/Constants/CognitiveServicesKeys.cs
+++ Source/XamSpeak/Constants/CognitiveServicesKeys.cs
@@ -2,11 +2,15 @@
 {
     public static class CognitiveServicesKeys
     {
-//#error Add your Cognitive Services API Keys
-        //Sign up for a free Computer Vision API Key: https://aka.ms/yqx7kz
-        public const string ComputerVisionAPIKey = "c65f5c8bc1184140bcb1e75ed1904215";
+        #error Add your Cognitive Services API Keys
+        //Sign up for a free Computer Vision API Key: https://aka.ms/CognitiveServices/ComputerVisionApi
 
-        //Sign up for a free Bing Spell Check API Key: https://aka.ms/Uis4df	
-        public const string BingSpellCheckAPIKey = "10aa65e1791c4af58743897524059def";
+        public const string ComputerVisionAPIKey = "Enter API Key Here";
+
+        //Sign up for a Bing Spell Check API Key: https://aka.ms/CognitiveServices/BingSpellCheckAPI
+        public const string BingSpellCheckAPIKey = "Enter API Key Here";
+
+        //Sign up for a free Speaker Recognition API Key:
+        public const string SpeakerRecognitionAPIKey = "Enter API Key Here";
     }
 }

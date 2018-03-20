48faf1cc47bad7148c6e79118bafaf2a961ffdf4
Brandon Minnick <brminnick@users.noreply.github.com>
2018-03-20 09:49:21 -0400

    Update CognitiveServicesConstants.cs

--- Source/XamSpeak/Constants/CognitiveServicesConstants.cs
+++ Source/XamSpeak/Constants/CognitiveServicesConstants.cs
@@ -3,10 +3,10 @@
     public static class CognitiveServicesConstants
     {
 #error Add your Cognitive Services API Keys
-        //Sign up for a free Computer Vision API Key: https://aka.ms/yqx7kz
+        //Sign up for a free Computer Vision API Key: https://aka.ms/cognitiveservices/computervisionapi
         public const string ComputerVisionAPIKey = "Enter Your API Key Here";
 
-        //Sign up for a free Bing Spell Check API Key: https://aka.ms/Uis4df	
+        //Sign up for a free Bing Spell Check API Key: https://aka.ms/CognitiveServices/BingSpellCheckAPI
         public const string BingSpellCheckAPIKey = "Enter You API Key Here";
     }
 }

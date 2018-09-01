2959630bb771c81b0ae6c240a8b079cb74dd5181
brminnick <brminnick@users.noreply.github.com>
2018-09-01 14:49:28 -0700

    Updated NuGet Packages

--- Source/XamSpeak/Services/OCRServices.cs
+++ Source/XamSpeak/Services/OCRServices.cs
@@ -8,138 +8,136 @@ using System.Collections.Generic;
 using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
 using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
 
-using Newtonsoft.Json;
-
 using Plugin.Media.Abstractions;
 
 namespace XamSpeak
 {
-	public static class OCRServices
-	{
+    public static class OCRServices
+    {
         #region Constant Fields
-        readonly static Lazy<ComputerVisionAPI> _computerVisionApiClientHolder =
-            new Lazy<ComputerVisionAPI>(() => new ComputerVisionAPI(new ApiKeyServiceClientCredentials(CognitiveServicesConstants.ComputerVisionAPIKey)));
-		#endregion
+        readonly static Lazy<ComputerVisionClient> _computerVisionApiClientHolder =
+            new Lazy<ComputerVisionClient>(() => new ComputerVisionClient(new ApiKeyServiceClientCredentials(CognitiveServicesConstants.ComputerVisionAPIKey)) { Endpoint = CognitiveServicesConstants.ComputerVisionBaseUrl });
+        #endregion
 
-		#region Events 
-		public static event EventHandler InvalidComputerVisionAPIKey;
-		#endregion
+        #region Events 
+        public static event EventHandler InvalidComputerVisionAPIKey;
+        #endregion
 
-		#region Properties
-		static ComputerVisionAPI ComputerVisionApiClient => _computerVisionApiClientHolder.Value;
-		#endregion
+        #region Properties
+        static ComputerVisionClient ComputerVisionApiClient => _computerVisionApiClientHolder.Value;
+        #endregion
 
-		#region Methods
-		public static async Task<OcrResult> GetOcrResultsFromMediaFile(MediaFile mediaFile)
-		{
-			try
-			{
-				return await ComputerVisionApiClient.RecognizePrintedTextInStreamAsync(true, ConverterHelpers.ConvertMediaFileToStream(mediaFile, false)).ConfigureAwait(false);
-			}
-			catch (ComputerVisionErrorException e) when (e.Response.StatusCode.Equals(HttpStatusCode.Unauthorized))
-			{
-				DebugHelpers.PrintException(e);
+        #region Methods
+        public static async Task<OcrResult> GetOcrResultsFromMediaFile(MediaFile mediaFile)
+        {
+            try
+            {
+                return await ComputerVisionApiClient.RecognizePrintedTextInStreamAsync(true, ConverterHelpers.ConvertMediaFileToStream(mediaFile, false)).ConfigureAwait(false);
+            }
+            catch (ComputerVisionErrorException e) when (e.Response.StatusCode.Equals(HttpStatusCode.Unauthorized))
+            {
+                DebugHelpers.PrintException(e);
 
-				OnInvalidComputerVisionAPIKey();
+                OnInvalidComputerVisionAPIKey();
 
-				throw;
-			}
-		}
+                throw;
+            }
+        }
 
-		public static List<string> GetTextFromOcrResults(OcrResult ocrResults)
-		{
-			var ocrModelList = new List<OcrTextLocationModel>();
+        public static List<string> GetTextFromOcrResults(OcrResult ocrResults)
+        {
+            var ocrModelList = new List<OcrTextLocationModel>();
 
-			foreach (var region in ocrResults.Regions)
-			{
-				foreach (var line in region.Lines)
-				{
-					var lineStringBuilder = new StringBuilder();
+            foreach (var region in ocrResults.Regions)
+            {
+                foreach (var line in region.Lines)
+                {
+                    var lineStringBuilder = new StringBuilder();
 
-					foreach (var word in line.Words)
-					{
-						lineStringBuilder.Append(word.Text);
-						lineStringBuilder.Append(" ");
-					}
-					var (left, top, width, height) = ConvertBoundingBox(line.BoundingBox);
-					ocrModelList.Add(new OcrTextLocationModel(lineStringBuilder.ToString(), top, left));
-				}
-			}
+                    foreach (var word in line.Words)
+                    {
+                        lineStringBuilder.Append(word.Text);
+                        lineStringBuilder.Append(" ");
+                    }
+                    var (left, top, width, height) = ConvertBoundingBox(line.BoundingBox);
+                    ocrModelList.Add(new OcrTextLocationModel(lineStringBuilder.ToString(), top, left));
+                }
+            }
 
-			return CreateStringFromOcrModelList(ocrModelList);
-		}
+            return CreateStringFromOcrModelList(ocrModelList);
+        }
 
-		static List<string> CreateStringFromOcrModelList(List<OcrTextLocationModel> ocrModelList)
-		{
-			if (ocrModelList is null || ocrModelList.Count <= 0)
-				return new List<string>();
+        static List<string> CreateStringFromOcrModelList(List<OcrTextLocationModel> ocrModelList)
+        {
+            if (ocrModelList is null || ocrModelList.Count <= 0)
+                return new List<string>();
 
-			var stringList = new List<string>();
-			var stringBuilder = new StringBuilder();
+            var stringList = new List<string>();
+            var stringBuilder = new StringBuilder();
 
-			var maximumTop = ocrModelList.OrderBy(x => x.Top).FirstOrDefault()?.Top;
+            var maximumTop = ocrModelList.OrderBy(x => x.Top).FirstOrDefault()?.Top;
 
-			var sortedOcrModelList = ocrModelList.OrderBy(x => x.Top).ThenBy(x => x.Left).ToList();
+            var sortedOcrModelList = ocrModelList.OrderBy(x => x.Top).ThenBy(x => x.Left).ToList();
 
-			var previousTop = 0.0;
-			foreach (OcrTextLocationModel ocrModel in sortedOcrModelList)
-			{
-				var percentageBelowPreviousOcrModel = (ocrModel.Top - previousTop) / maximumTop;
+            var previousTop = 0.0;
+            foreach (OcrTextLocationModel ocrModel in sortedOcrModelList)
+            {
+                var percentageBelowPreviousOcrModel = (ocrModel.Top - previousTop) / maximumTop;
 
-				if (percentageBelowPreviousOcrModel <= 0.01)
-				{
-					stringBuilder.Append(" ");
-					stringBuilder.Append($"{ocrModel.Text}");
-				}
-				else
-				{
-					if (!string.IsNullOrEmpty(stringBuilder.ToString()))
-						stringList.Add(stringBuilder.ToString());
+                if (percentageBelowPreviousOcrModel <= 0.01)
+                {
+                    stringBuilder.Append(" ");
+                    stringBuilder.Append($"{ocrModel.Text}");
+                }
+                else
+                {
+                    if (!string.IsNullOrEmpty(stringBuilder.ToString()))
+                        stringList.Add(stringBuilder.ToString());
 
-					stringBuilder.Clear();
-					stringBuilder.Append($"{ocrModel.Text}");
-				}
+                    stringBuilder.Clear();
+                    stringBuilder.Append($"{ocrModel.Text}");
+                }
 
-				previousTop = ocrModel.Top;
+                previousTop = ocrModel.Top;
 
-				if (sortedOcrModelList.LastOrDefault().Equals(ocrModel))
-					stringList.Add(stringBuilder.ToString());
-			}
+                if (sortedOcrModelList.LastOrDefault().Equals(ocrModel))
+                    stringList.Add(stringBuilder.ToString());
+            }
 
-			return stringList;
-		}
+            return stringList;
+        }
 
-		static (int left, int top, int width, int height) ConvertBoundingBox(string boundingBox)
-		{
-			var boundingBoxArray = boundingBox.Split(',');
+        static (int left, int top, int width, int height) ConvertBoundingBox(string boundingBox)
+        {
+            var boundingBoxArray = boundingBox.Split(',');
 
 
-			int.TryParse(boundingBoxArray[0], out int left);
-			int.TryParse(boundingBoxArray[1], out int top);
-			int.TryParse(boundingBoxArray[2], out int width);
-			int.TryParse(boundingBoxArray[3], out int height);
+            int.TryParse(boundingBoxArray[0], out int left);
+            int.TryParse(boundingBoxArray[1], out int top);
+            int.TryParse(boundingBoxArray[2], out int width);
+            int.TryParse(boundingBoxArray[3], out int height);
 
-			return (left, top, width, height);
-		}
+            return (left, top, width, height);
+        }
 
-		static void OnInvalidComputerVisionAPIKey() =>
-			InvalidComputerVisionAPIKey?.Invoke(null, EventArgs.Empty);
-		#endregion
+        static void OnInvalidComputerVisionAPIKey() =>
+            InvalidComputerVisionAPIKey?.Invoke(null, EventArgs.Empty);
+        #endregion
 
-		#region Classes
-		class OcrTextLocationModel
-		{
-			public OcrTextLocationModel(string text, int top, int left)
-			{
-				Text = text;
-				Top = top;
-				Left = left;
-			}
+        #region Classes
+        class OcrTextLocationModel
+        {
+            public OcrTextLocationModel(string text, int top, int left)
+            {
+                Text = text;
+                Top = top;
+                Left = left;
+            }
 
-			public string Text { get; }
-			public double Top { get; }
-			public double Left { get; }
-		}
-		#endregion
-	}
+            public string Text { get; }
+            public double Top { get; }
+            public double Left { get; }
+        }
+        #endregion
+    }
 }
--- Source/XamSpeak/Services/SpellCheckServices.cs
+++ Source/XamSpeak/Services/SpellCheckServices.cs
@@ -9,90 +9,90 @@ using Microsoft.Azure.CognitiveServices.Language.SpellCheck.Models;
 
 namespace XamSpeak
 {
-	static class SpellCheckServices
-	{
-		#region Constant Fields
-		const double _minimumConfidenceScore = 0.80;
+    static class SpellCheckServices
+    {
+        #region Constant Fields
+        const double _minimumConfidenceScore = 0.80;
         static readonly Lazy<SpellCheckClient> _spellCheckApiClient =
             new Lazy<SpellCheckClient>(() => new SpellCheckClient(new ApiKeyServiceClientCredentials(CognitiveServicesConstants.BingSpellCheckAPIKey)));
-		#endregion
+        #endregion
 
-		#region Events
-		public static event EventHandler InvalidBingSpellCheckAPIKey;
-		public static event EventHandler Error429_TooManySpellCheckAPIRequests;
-		#endregion
+        #region Events
+        public static event EventHandler InvalidBingSpellCheckAPIKey;
+        public static event EventHandler Error429_TooManySpellCheckAPIRequests;
+        #endregion
 
-		#region Properties
-		static SpellCheckClient SpellCheckApiClient => _spellCheckApiClient.Value;
-		#endregion
+        #region Properties
+        static SpellCheckClient SpellCheckApiClient => _spellCheckApiClient.Value;
+        #endregion
 
-		#region Methods
-		public static async Task<List<string>> GetSpellCheckedStringList(List<string> stringList)
-		{
-			int listIndex = 0;
-			var correctedLineItemList = new List<string>();
+        #region Methods
+        public static async Task<List<string>> GetSpellCheckedStringList(List<string> stringList)
+        {
+            int listIndex = 0;
+            var correctedLineItemList = new List<string>();
 
-			foreach (string lineItem in stringList)
-			{
-				correctedLineItemList.Add(lineItem);
+            foreach (string lineItem in stringList)
+            {
+                correctedLineItemList.Add(lineItem);
 
-				var misspelledWordList = await SpellCheckString(lineItem).ConfigureAwait(false);
+                var misspelledWordList = await SpellCheckString(lineItem).ConfigureAwait(false);
 
-				foreach (var misspelledWord in misspelledWordList)
-				{
-					var firstSuggestion = misspelledWord.Suggestions.FirstOrDefault();
+                foreach (var misspelledWord in misspelledWordList)
+                {
+                    var firstSuggestion = misspelledWord.Suggestions.FirstOrDefault();
 
-					if (firstSuggestion?.Score >= _minimumConfidenceScore)
-					{
-						var correctedLineItem = correctedLineItemList[listIndex].Replace(misspelledWord.Token, firstSuggestion?.Suggestion);
+                    if (firstSuggestion?.Score >= _minimumConfidenceScore)
+                    {
+                        var correctedLineItem = correctedLineItemList[listIndex].Replace(misspelledWord.Token, firstSuggestion?.Suggestion);
 
-						correctedLineItemList[listIndex] = correctedLineItem;
-					}
-				}
+                        correctedLineItemList[listIndex] = correctedLineItem;
+                    }
+                }
 
-				listIndex++;
-			}
+                listIndex++;
+            }
 
-			return correctedLineItemList;
-		}
+            return correctedLineItemList;
+        }
 
 
-		static async Task<List<SpellingFlaggedToken>> SpellCheckString(string text)
-		{
-			try
-			{
-				var spellCheckModel = await SpellCheckApiClient.SpellCheckerAsync(text).ConfigureAwait(false);
+        static async Task<List<SpellingFlaggedToken>> SpellCheckString(string text)
+        {
+            try
+            {
+                var spellCheckModel = await SpellCheckApiClient.SpellCheckerAsync(text).ConfigureAwait(false);
 
-				return new List<SpellingFlaggedToken>(spellCheckModel.FlaggedTokens);
-			}
-			catch (ErrorResponseException e) when (e.Response.StatusCode.Equals(HttpStatusCode.Unauthorized))
-			{
-				DebugHelpers.PrintException(e);
-				OnInvalidBingSpellCheckAPIKey();
-            
-				throw;
-			}
-			catch (ErrorResponseException e) when (e.Response.StatusCode.Equals(429))
-			{
-				DebugHelpers.PrintException(e);
-				OnError429_TooManySpellCheckAPIRequests();
-
-				throw;
-			}
-            catch(ArgumentNullException e)
-			{
-				DebugHelpers.PrintException(e);
+                return new List<SpellingFlaggedToken>(spellCheckModel.FlaggedTokens);
+            }
+            catch (ErrorResponseException e) when (e.Response.StatusCode.Equals(HttpStatusCode.Unauthorized))
+            {
+                DebugHelpers.PrintException(e);
                 OnInvalidBingSpellCheckAPIKey();
 
-				throw;
-			}
-		}
+                throw;
+            }
+            catch (ErrorResponseException e) when (e.Response.StatusCode.Equals(429))
+            {
+                DebugHelpers.PrintException(e);
+                OnError429_TooManySpellCheckAPIRequests();
 
-		static void OnInvalidBingSpellCheckAPIKey() =>
-			InvalidBingSpellCheckAPIKey?.Invoke(null, EventArgs.Empty);
+                throw;
+            }
+            catch (ArgumentNullException e)
+            {
+                DebugHelpers.PrintException(e);
+                OnInvalidBingSpellCheckAPIKey();
 
-		static void OnError429_TooManySpellCheckAPIRequests() =>
-			Error429_TooManySpellCheckAPIRequests?.Invoke(null, EventArgs.Empty);
-		#endregion
-	}
+                throw;
+            }
+        }
+
+        static void OnInvalidBingSpellCheckAPIKey() =>
+            InvalidBingSpellCheckAPIKey?.Invoke(null, EventArgs.Empty);
+
+        static void OnError429_TooManySpellCheckAPIRequests() =>
+            Error429_TooManySpellCheckAPIRequests?.Invoke(null, EventArgs.Empty);
+        #endregion
+    }
 }

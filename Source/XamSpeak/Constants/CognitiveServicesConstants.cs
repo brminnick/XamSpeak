448e2bec34339710499e6303c46b1dec5710b3c6
brminnick <brminnick@users.noreply.github.com>
2018-09-01 14:50:42 -0700

    Added ComputerVisionBaseUrl

--- Source/XamSpeak/Constants/CognitiveServicesConstants.cs
+++ Source/XamSpeak/Constants/CognitiveServicesConstants.cs
@@ -2,9 +2,12 @@
 {
     public static class CognitiveServicesConstants
     {
-#error Add your Cognitive Services API Keys
+#error Missing Cognitive Services Constants
         //Sign up for a free Computer Vision API Key: https://aka.ms/CognitiveServices/ComputerVisionApi
         public const string ComputerVisionAPIKey = "Enter Your API Key Here";
+
+        //ComputerVisionBaseUrl example: https://westus.api.cognitive.microsoft.com/
+        public const string ComputerVisionBaseUrl = "Enter Your Computer Vision Base Url Here";
 
         //Sign up for a free Bing Spell Check API Key: https://aka.ms/CognitiveServices/BingSpellCheckAPI
         public const string BingSpellCheckAPIKey = "Enter You API Key Here";
